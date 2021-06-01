0a22ded8c4cfb9dd4a5968a04dc57705618587ed
shhumaan <48822497+shhumaan@users.noreply.github.com>
2021-06-01 21:53:45 +0530

    last commit

--- lib/secrets.dart
+++
@@ -1,6 +0,0 @@
-get secret => {
-      "consumerKey": "ck_b06de2efba8d1835eca157fdbc5cee8c3ef0a40d",
-      "consumerSecret": "cs_fbbaa10645700c0e54bf5a1c2f73d91db49011fd",
-      "baseURL": "https://demo.techieville.in"
-    };
-//TODO: 
